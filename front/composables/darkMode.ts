c1cdc653987882e29d6203e94620072fbadfdb58
pabloflores465 <pablopolis2016@gmai.com>
2025-02-17 04:12:15 -0600

    adding dark mode

--- front/composables/darkMode.ts
+++ front/composables/darkMode.ts
@@ -1,24 +1,20 @@
-export const darkMode = () =>
-  useState("darkMode", () => {
-    if (
-      import.meta.client &&
-      typeof window !== "undefined" &&
-      window.localStorage
-    ) {
+export const darkMode = () => {
+  const state = useState("darkMode", () => false);
+
+  onMounted(() => {
+    if (window && window.localStorage) {
       const stored = localStorage.getItem("darkMode");
-      return stored ? JSON.parse(stored) : false;
+      state.value = stored ? JSON.parse(stored) : false;
     }
-    return false;
   });
+
+  return state;
+};
 
 export const toggleDark = () => {
   const dark = darkMode();
   dark.value = !dark.value;
-  if (
-    import.meta.client &&
-    typeof window !== "undefined" &&
-    window.localStorage
-  ) {
+  if (typeof window !== "undefined" && window.localStorage) {
     localStorage.setItem("darkMode", JSON.stringify(dark.value));
   }
 };
