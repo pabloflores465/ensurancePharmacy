44d571ef023e4425a08c953dd8bc978ffb8ddcbe
pabloflores465 <pablopolis2016@gmai.com>
2025-02-19 22:53:16 -0600

    Making dark mode consistent

--- front/pages/prescription.vue
+++ front/pages/prescription.vue
@@ -262,24 +262,49 @@ const prescriptions: Ref<
 
 <template>
   <main
-    class="bg-image-[url('https://cdn.prod.website-files.com/6466101d017ab9d60c8d0137/668ce90cd1d21a2f4e8a8536_Repeat%20Prescriptions.jpg')]"
+    class="bg-image-[url('https://cdn.prod.website-files.com/6466101d017ab9d60c8d0137/668ce90cd1d21a2f4e8a8536_Repeat%20Prescriptions.jpg')] max-sm:flex-col max-sm:items-center max-sm:justify-center max-sm:gap-4 max-sm:px-2 max-sm:py-8"
   >
     <div
       v-for="prescription in prescriptions"
-      class="card gap-4 max-sm:mx-2 max-sm:flex-col md:grid md:grid-flow-row md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4"
+      class="card lg:align-center gap-4 transition duration-300 hover:scale-105 max-sm:mx-2 max-sm:flex-col md:flex-row lg:align-middle"
     >
       <p class="text-title title mb-4">Hospital: {{ prescription.hospital }}</p>
       <div class="text-md text-primary mb-4 flex">
+        <svg
+          class="me-2"
+          xmlns="http://www.w3.org/2000/svg"
+          height="24px"
+          viewBox="0 -960 960 960"
+          width="24px"
+          fill="currentColor"
+        >
+          <path
+            d="M480-480q-66 0-113-47t-47-113q0-66 47-113t113-47q66 0 113 47t47 113q0 66-47 113t-113 47ZM160-160v-112q0-34 17.5-62.5T224-378q62-31 126-46.5T480-440q66 0 130 15.5T736-378q29 15 46.5 43.5T800-272v112H160Zm80-80h480v-32q0-11-5.5-20T700-306q-54-27-109-40.5T480-360q-56 0-111 13.5T260-306q-9 5-14.5 14t-5.5 20v32Zm240-320q33 0 56.5-23.5T560-640q0-33-23.5-56.5T480-720q-33 0-56.5 23.5T400-640q0 33 23.5 56.5T480-560Zm0-80Zm0 400Z"
+          />
+        </svg>
         <p class="me-2 font-semibold">Usuario:</p>
+
         {{ prescription.patient }}
       </div>
       <div class="text-secondary mb-6 flex text-sm">
+        <svg
+          class="me-2"
+          xmlns="http://www.w3.org/2000/svg"
+          height="20px"
+          viewBox="0 -960 960 960"
+          width="20px"
+          fill="currentColor"
+        >
+          <path
+            d="M680-320q-50 0-85-35t-35-85q0-50 35-85t85-35q50 0 85 35t35 85q0 50-35 85t-85 35Zm0-80q17 0 28.5-11.5T720-440q0-17-11.5-28.5T680-480q-17 0-28.5 11.5T640-440q0 17 11.5 28.5T680-400ZM440-40v-116q0-21 10-39.5t28-29.5q32-19 67.5-31.5T618-275l62 75 62-75q37 6 72 18.5t67 31.5q18 11 28.5 29.5T920-156v116H440Zm79-80h123l-54-66q-18 5-35 13t-34 17v36Zm199 0h122v-36q-16-10-33-17.5T772-186l-54 66Zm-76 0Zm76 0Zm-518 0q-33 0-56.5-23.5T120-200v-560q0-33 23.5-56.5T200-840h560q33 0 56.5 23.5T840-760v200q-16-20-35-38t-45-24v-138H200v560h166q-3 11-4.5 22t-1.5 22v36H200Zm80-480h280q26-20 57-30t63-10v-40H280v80Zm0 160h200q0-21 4.5-41t12.5-39H280v80Zm0 160h138q11-9 23.5-16t25.5-13v-51H280v80Zm-80 80v-560 137-17 440Zm480-240Z"
+          />
+        </svg>
         <p class="me-2 font-semibold">Doctor:</p>
         {{ prescription.doctor }}
       </div>
       <button
         @click="() => (prescription.show = !prescription.show)"
-        class="btn flex"
+        class="btn flex justify-center"
       >
         <svg
           class="me-2"
@@ -295,13 +320,49 @@ const prescriptions: Ref<
         </svg>
         See More Details
       </button>
-      <Modal v-model:show="prescription.show">
-        <div class="title">
-          dsfsghjfgdfgsdafghjfhdfsfadgfhgjhhdgsfaddgfhgjhfhdgsfadgfhgjhdghgjhola
-          mundo
-        </div>
-      </Modal>
     </div>
+    <Modal
+      v-for="prescription in prescriptions"
+      v-model:show="prescription.show"
+    >
+      <p class="title mb-6">
+        {{ prescription.hospital }}
+      </p>
+
+      <div class="text-md text-primary mb-4 flex">
+        <svg
+          class="me-2"
+          xmlns="http://www.w3.org/2000/svg"
+          height="24px"
+          viewBox="0 -960 960 960"
+          width="24px"
+          fill="currentColor"
+        >
+          <path
+            d="M480-480q-66 0-113-47t-47-113q0-66 47-113t113-47q66 0 113 47t47 113q0 66-47 113t-113 47ZM160-160v-112q0-34 17.5-62.5T224-378q62-31 126-46.5T480-440q66 0 130 15.5T736-378q29 15 46.5 43.5T800-272v112H160Zm80-80h480v-32q0-11-5.5-20T700-306q-54-27-109-40.5T480-360q-56 0-111 13.5T260-306q-9 5-14.5 14t-5.5 20v32Zm240-320q33 0 56.5-23.5T560-640q0-33-23.5-56.5T480-720q-33 0-56.5 23.5T400-640q0 33 23.5 56.5T480-560Zm0-80Zm0 400Z"
+          />
+        </svg>
+        <p class="me-2 font-semibold">Usuario:</p>
+
+        {{ prescription.patient }}
+      </div>
+      <div class="text-secondary mb-6 flex text-sm">
+        <svg
+          class="me-2"
+          xmlns="http://www.w3.org/2000/svg"
+          height="20px"
+          viewBox="0 -960 960 960"
+          width="20px"
+          fill="currentColor"
+        >
+          <path
+            d="M680-320q-50 0-85-35t-35-85q0-50 35-85t85-35q50 0 85 35t35 85q0 50-35 85t-85 35Zm0-80q17 0 28.5-11.5T720-440q0-17-11.5-28.5T680-480q-17 0-28.5 11.5T640-440q0 17 11.5 28.5T680-400ZM440-40v-116q0-21 10-39.5t28-29.5q32-19 67.5-31.5T618-275l62 75 62-75q37 6 72 18.5t67 31.5q18 11 28.5 29.5T920-156v116H440Zm79-80h123l-54-66q-18 5-35 13t-34 17v36Zm199 0h122v-36q-16-10-33-17.5T772-186l-54 66Zm-76 0Zm76 0Zm-518 0q-33 0-56.5-23.5T120-200v-560q0-33 23.5-56.5T200-840h560q33 0 56.5 23.5T840-760v200q-16-20-35-38t-45-24v-138H200v560h166q-3 11-4.5 22t-1.5 22v36H200Zm80-480h280q26-20 57-30t63-10v-40H280v80Zm0 160h200q0-21 4.5-41t12.5-39H280v80Zm0 160h138q11-9 23.5-16t25.5-13v-51H280v80Zm-80 80v-560 137-17 440Zm480-240Z"
+          />
+        </svg>
+        <p class="me-2 font-semibold">Doctor:</p>
+        {{ prescription.doctor }}
+      </div>
+    </Modal>
   </main>
 </template>
 
